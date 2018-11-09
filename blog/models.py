bc3a5219cfc40a0204eb4db81095bfe14b943c05
Andydenike <denikeandrew@gmail.com>
2018-11-09 12:10:16 -0800

    Created Post class model with fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
-# Create your models here.
+#each class is its own table in the db
+#each attribute will be its own field in db 
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	#if user is deleted, delete Posts 
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
