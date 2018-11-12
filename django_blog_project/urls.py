156a0a74ce033f638f5b31894f82c30fe879b1bc
Andydenike <denikeandrew@gmail.com>
2018-11-11 16:56:08 -0800

    Added urlpattern route for register / passed users.views

--- django_blog_project/urls.py
+++ django_blog_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from users import views as user_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')), #empty string will default url to this urls 
     #checks for pattern, passes onto potential include with the rest of url pattern
 ]
