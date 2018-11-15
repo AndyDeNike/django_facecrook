d2ac854a451857bbf2db8e7e66e7f305017d072a
Andydenike <denikeandrew@gmail.com>
2018-11-14 19:15:02 -0800

    Set a static urlpattern for MEDIA

--- django_blog_project/urls.py
+++ django_blog_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views #anytime you import views specify the type of view 'as'
 from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static 
 from users import views as user_views 
 
 urlpatterns = [
@@ -26,4 +28,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')), #empty string will default url to this urls 
     #checks for pattern, passes onto potential include with the rest of url pattern
-]
+] 
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
