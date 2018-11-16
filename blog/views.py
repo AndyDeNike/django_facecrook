43e8304b78bab466dd0e8487e811c94b92e10b77
Andydenike <denikeandrew@gmail.com>
2018-11-15 21:40:29 -0800

    Created PostCreateView(CreateView) / created form_valid method to assign self.request,user to form.instance.author and validate form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView	
 from .models import Post
 #from django.http import HttpResponse
 
@@ -21,7 +21,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
 	model = Post 
-	
+
+class PostCreateView(CreateView):
+	model = Post 
+	fields = ['title', 'content']
+
+	# Before you submit form, take instance and set it to current user
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
