d5685a5ad3ca14e834e21138c2561fad2b10507e
Andydenike <denikeandrew@gmail.com>
2018-11-15 18:43:05 -0800

    Created PostDetailView(DetailView)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 #from django.http import HttpResponse
 
@@ -6,10 +7,21 @@ from .models import Post
 def home(request):
 	context = {
 		'posts': Post.objects.all() 
-	}
+	} 
 
 	#render returns HttpResponse
 	return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+	model = Post 
+	template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html 
+	context_object_name = 'posts'  
+	ordering = ['-date_posted']  #setting ordering attribute on the field you want ordered (- reverses order)
+
+class PostDetailView(DetailView):
+	model = Post 
+	
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
