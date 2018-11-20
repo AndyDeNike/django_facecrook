c0fc719436d2531de96004ab6abf5efa6d8fd5ed
Andydenike <denikeandrew@gmail.com>
2018-11-20 14:31:16 -0800

    Editing aws s3 test

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -145,12 +145,22 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
-# AWS_S3_REGION_NAME = 'us-east-2'
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 import dj_database_url
 db_from_env = dj_database_url.config()
