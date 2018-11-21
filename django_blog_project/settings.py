2d342cbb249dea8a91cfc27990334ff84c0cbc35
Andydenike <denikeandrew@gmail.com>
2018-11-20 16:09:13 -0800

    MEDIA settings test 2

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -129,7 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/' #accessing media from browser 
 
 # s3bucketname = S3Connection(os.environ['S3_BUCKET'])
-MEDIA_URL = 'https://s3.amazonaws.com:443/%s/media/' % AWS_STORAGE_BUCKET_NAME
 # STATIC_URL = 'https://s3.amazonaws.com:443/%s/static/' % s3bucketname
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -159,6 +158,8 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
+
+MEDIA_URL = 'https://s3.amazonaws.com:443/%s/media/' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
