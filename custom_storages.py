30272778057e3ea58b77f395df1c0b2b962038d4
Andydenike <denikeandrew@gmail.com>
2018-11-27 21:13:32 -0800

    Test custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -3,3 +3,20 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+    
+class CachedS3BotoStorage(S3BotoStorage):
+    """
+    S3 storage backend that saves the files both remotely and locally.
+
+    See http://django_compressor.readthedocs.org/en/latest/remote-storages/
+    """
+    def __init__(self, *args, **kwargs):
+        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "django.core.files.storage.FileSystemStorage")()
+
+    def save(self, name, content):
+        name = super(CachedS3BotoStorage, self).save(name, content)
+        self.local_storage._save(name, content)
+        return name
