12290147b1330896c9f9745acd24cd02c6ece880
Andydenike <denikeandrew@gmail.com>
2018-11-27 21:43:57 -0800

    Test

--- custom_storages.py
+++ custom_storages.py
@@ -5,15 +5,7 @@ class MediaStorage(S3Boto3Storage):
 
     location = settings.MEDIAFILES_LOCATION
 
-    def _clean_name(self, name):
-        return name
 
-    def _normalize_name(self, name):
-        if not name.endswith('/'):
-            name += "/"
-
-        name += self.location
-        return name
 
 
 # class CachedS3BotoStorage(S3Boto3Storage):
