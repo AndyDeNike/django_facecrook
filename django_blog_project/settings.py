134742467d292e12ba7c495b433a9529cb68ea6c
Andydenike <denikeandrew@gmail.com>
2018-11-28 13:39:24 -0800

    Test

--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #location where media will be sent 
-#MEDIA_URL = '/media/' #accessing media from browser 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #location where media will be sent 
+MEDIA_URL = '/media/' #accessing media from browser 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
