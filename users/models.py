253ac392f94b1af1f12e5c9304eff6bd028bdbae
Andydenike <denikeandrew@gmail.com>
2018-11-27 21:25:52 -0800

    Test

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.core.files.storage import default_storage as storage
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	image = models.ImageField(default='default.jpg', upload_to='https://s3-us-west-1.amazonaws.com/djangoblogbucket/media/profile_pics/')
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
@@ -14,7 +14,8 @@ class Profile(models.Model):
 		super().save()
 
 		#img = Image.open(self.image.path)
-		img = Image.open(storage.open(self.image.path))
+		img = Image.open(storage.open(self.image.name))
+		#img = Image.open(storage.open(self.image.path))
 
 		if img.height > 300 or img.width > 300:
 			output_size = (300, 300)
