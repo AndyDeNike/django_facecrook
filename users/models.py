41ec17c482dd579782c3866ce792c32f38371317
Andydenike <denikeandrew@gmail.com>
2018-11-28 13:22:50 -0800

    Test

--- users/models.py
+++ users/models.py
@@ -17,9 +17,9 @@ class Profile(models.Model):
 		img = Image.open(storage.open(self.image.name))
 		#img = Image.open(storage.open(self.image.path))
 
-		#if img.height > 300 or img.width > 300:
-			#output_size = (300, 300)
-			#img.thumbnail(output_size)
-		img.save(self.image.name)
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save(self.image.name)
 
 			
